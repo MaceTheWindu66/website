8ebdd1243bb7f12ea99d7e3b8a5c4332e36aebb2
MaceTheWindu66 <madduxberry31@gmail.com>
2024-08-04 11:26:25 -0400

    Big updates to website structure

---
+++ react-typescript-tut/src/App.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className = "container">
+          <div className = "left-content">
+            <div className = "left-content-header"> Hello! I'm Maddux </div>
+            <div className = "left-content-body"> 
+              I'm a Computer Science student at Worcester Polytechnic Institute, pursuing a Bachelor's and Master's 
+              degree. I am also currently seeking an internship in the field of Computer Science!
+
+
+            </div>
+             
+          </div>
+          <div className = "right-content"> Image Here </div>
+    </div>
+  );
+}
+
+export default App;
