25b3f605f6c8db8e699f07523cf767d45a5f8db4
MaceTheWindu66 <madduxberry31@gmail.com>
2024-08-05 23:33:53 -0400

    More fix attempts

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ import './App.css';
 
 function App() {
   return (
-    <Router>
+    <Router basename="/website  ">
       <nav className="navbar">
         <div className="navbar-links">
           <Link to="/">Home</Link>
