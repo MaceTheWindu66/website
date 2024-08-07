6adc6603c6b1ecc32bae627c0f435c8e9a0b2526
MaceTheWindu66 <madduxberry31@gmail.com>
2024-08-07 14:12:33 -0400

    Home page struture is complete

--- src/Home.tsx
+++ src/Home.tsx
@@ -2,12 +2,13 @@ import React from 'react';
 import {motion} from 'framer-motion';
 import './Home.css';
 import { Hidden } from '@mui/material';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 
 const Home = () => {
   return (
     <div style={{ paddingTop: '5vh' }}>
       <div className = "container">
-          <div className = "section">
+          <div className = "section" style={{marginBottom: '2vh'}}>
             <div className = "left-content">
                 <div className = "content-header"> Hello! I'm Maddux! </div>
                 <div className = "content-body"> I'm currently a third year student at Worcester Polytechnic Institute
@@ -35,22 +36,34 @@ const Home = () => {
                 </div>
             </div>
             <div className = "right-content"> 
-              <div className = "content-header"> CS Related Skills </div>
-                    <div className = "content-body"> 
-                      <div className = "scroller-box">
-                        <div>Operating Systems</div>
-                        <div>Algorithms</div>
-                        <div>Data Structures</div>
-                        <div>Systems Programming</div>
-                        <div>Front-End Development</div>
-                        <div>Back-End Development</div>
-                        <div>Software Development</div>
-                        <div>Web Development</div>
-                        <div>Object Oriented Programming</div>
-                        <div>Parallel Programming</div>
-                        <div>Multithreaded Development</div>
-                      </div>
+            <div className = "content-header" style ={{marginBottom:'2vh'}}> Highlighted Projects </div>
+            <div className = "content-body">
+            <div className = "projects-box">
+                  <div className = "projects-content" style={{paddingRight: '30px'}}>
+                    <iframe
+                      width= '80%'
+                      height= '50%'
+                      src="https://www.youtube.com/embed/TLQI63xV8RQ"
+                      frameBorder="0"
+                      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                      allowFullScreen
+            >        </iframe>
+            </div>
+                  <div className = "projects-content"> For this project, I used Python and Pytorch
+                      to create a Deep Learning Neural Network Model that
+                      teaches itself how to play the classic game Snake.
+                  </div>
                 </div>
+                <div className = "projects-box">
+                  <div className = "projects-content">
+                    For my software engineering class I worked on a team of 11
+                    people as Project Manager and Full Stack Developer, and we were tasked
+                    with building this website for Brigham and Women's Hospital.
+                  </div>
+                </div>
+                <div style={{marginTop: '1vh', fontWeight: 500}}> See the rest of my Projects! <Link to="/projects" className="nav-button2">Projects</Link></div>
+
+            </div>
             </div>
             
           </div>
@@ -72,10 +85,27 @@ const Home = () => {
               </div>
             </div>
             <div className = "right-content">
+             
+              <div className = "content-header"> CS Related Skills </div>
+                    <div className = "content-body"> 
+                      <div className = "scroller-box">
+                        <div>Operating Systems</div>
+                        <div>Algorithms</div>
+                        <div>Data Structures</div>
+                        <div>Systems Programming</div>
+                        <div>Front-End Development</div>
+                        <div>Back-End Development</div>
+                        <div>Software Development</div>
+                        <div>Web Development</div>
+                        <div>Object Oriented Programming</div>
+                        <div>Parallel Programming</div>
+                        <div>Multithreaded Development</div>
+                      </div>
+                </div>
+              </div>
             </div>
           </div>
       </div>
-    </div>
   );
 };
 
