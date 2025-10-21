ad060f0108980d0cce2ea7596e95ea125b12b129
MaceTheWindu66 <madduxberry31@gmail.com>
2025-10-21 01:20:05 -0400

    Restoring repo locally

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+import HeroSection from './sections/HeroSection'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <HeroSection />
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/sections/HeroSection.tsx
@@ -0,0 +1,14 @@
+
+export default function HeroSection() {
+    
+    return (
+        <div>
+        <h1 className="text-3xl font-bold underline">
+            Hello world!
+            </h1>
+
+        </div>
+    );
+
+};
+
